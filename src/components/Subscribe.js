ee76f917f2277aefa1b43979f3ce73943747d086
Arunava Ghosh <102473837+its-ag@users.noreply.github.com>
2022-10-27 00:28:19 +0530

    Fix: Dark Mode

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -34,25 +34,25 @@ const Subscribe = () => {
 
   return (
     <>  
-      <div className="border border-gray-200 rounded p-6 my-4 w-full bg-gray-50">
-        <p className="text-gray-900 dark:text-gray-200 mb-6 text-lg md:text-xl">
-          Want to keep your brain engaged with great UI/UX learning content?
+      <div className="border border-blue-200 rounded p-6 my-4 w-full dark:border-gray-800 bg-blue-50 dark:bg-surface">
+        <p className="text-lg md:text-xl font-bold text-gray-900 dark:text-gray-100">
+          Subscribe to My Newsletter
         </p>
-        <p className="text-gray-800 dark:text-gray-400 mb-10 text-base">
-          Enter your email address and you willl be be added to my email newsletter, of which you can opt out any time.
+        <p className="my-1 text-gray-800 dark:text-gray-200">
+        Get emails from me about web development, tech, and early access to new articles.
         </p>
         <form className="relative my-4" onSubmit={subscribeMe}>
           <input
             onChange={changeEmail}
             aria-label="Email for newsletter"
-            placeholder="john@email.com"
+            placeholder="youareawesome@email.com"
             type="email"
             autoComplete="email"
             required
-            className="py-4 px-0 text-md bg-transparent w-9/12 text-gray-900 border-b-2 border-gray-600 dark:border-gray-400 dark:text-white focus:border-brand focus-visible:outline-none"
+            className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 pr-32"
           />
           <button
-            className="flex justify-center px-5 py-4 mt-8 bg-green-600 text-white font-bold text-lg"
+            className="flex items-center justify-center absolute right-1 top-1 px-4 pt-1 font-medium h-8 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded w-28"
             type="submit"
           >
             Subscribe
@@ -61,18 +61,18 @@ const Subscribe = () => {
         {success 
           ? <span className="flex items-center text-sm font-bold text-green-700">{success}</span> 
           : <span className="flex items-center text-sm font-bold text-red-800">{error}</span>}
-        <p className="text-xl text-gray-800 dark:text-gray-200">
-          { subscriberCount } subscribers . {issues && issues.length} {issues && issues.length > 1 ? 'issues' : 'issue'}
+        <p className="text-sm text-gray-800 dark:text-gray-200">
+          { subscriberCount } subscribers - {issues && issues.length} {issues && issues.length > 1 ? 'issues' : 'issue'}
         </p>
-        <p className="text-sm  text-center p-3 text-gray-800">
+        {/* <p className="text-sm  text-center p-3 text-gray-800">
           Well, you can subscribe to my upcoming Newsletter for real. Just give a valid email id above and verify 😀.
-        </p>
+        </p> */}
       </div>
       <div>
         <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black">
               Published Issues
         </h3>
-        <div className="flex flex-col bg-gray-200 p-4 underline">
+        {/* <div className="flex flex-col bg-gray-200 p-4 underline">
           <ul>
             {issues && issues
               .sort(
@@ -85,7 +85,7 @@ const Subscribe = () => {
                 <a key={index} href={issue.url} target="_blank" rel="noopener norefer noreferrer"> {issue.title} </a>
               ))}
           </ul>
-        </div>
+        </div> */}
       </div>
     </>
   );
