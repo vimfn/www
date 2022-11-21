877749ed8788da6ca8303b14cf206d7e659ebb5a
Arunava Ghosh <102473837+its-ag@users.noreply.github.com>
2022-11-21 21:08:35 +0530

    Typo Fix

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -37,7 +37,7 @@ const Post = defineDocumentType(() => ({
       of: {
         type: "string",
       },
-      default: ["linh tinh"],
+      default: ["Uncategorized"],
       description: "Tags for the post",
       required: false,
     },
