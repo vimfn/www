6aec86fb364962b492eafdc8b85a77caaaf0ff8b
Arunava Ghosh <arnvgh@gmail.com>
2023-12-31 12:16:28 +0530

    rm -rf * (old source code)

--- data/headerNavLinks.ts
+++
@@ -1,9 +0,0 @@
-const headerNavLinks = [
-  { href: "/blog", title: "Blog" },
-  { href: "/tag", title: "Tags" },
-  { href: "/snippet", title: "Snippets" },
-  { href: "/about", title: "About" },
-  { href: "/newsletter", title: "Newsletter" },
-];
-
-export default headerNavLinks;
