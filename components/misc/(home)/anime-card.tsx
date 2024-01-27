b00f4b36d183ca42b459ced4a70c0d430bbee894
Arunava Ghosh <arnvgh@gmail.com>
2024-01-27 10:32:15 +0530

    feat: 👾

--- components/misc/(home)/anime-card.tsx
+++ components/misc/(home)/anime-card.tsx
@@ -11,6 +11,8 @@ const AnimeCard = () => {
           className="absolute object-cover inset-0 w-full h-full -z-40 rounded-lg  dark:brightness-75"
           src={animePic}
           alt="anime"
+          placeholder="blur"
+          priority
         />
 
         <ArrowUpRight className="absolute text-white transition-all group-hover:scale-105 top-2 right-2 border border-white bg-black rounded-full p-1" />
--- components/misc/(home)/map.tsx
+++ components/misc/(home)/map.tsx
@@ -5,7 +5,7 @@ const Map = () => {
   return (
     <div className="h-36">
       <div className="relative">
-        <Image src={map} alt="" className="rounded-lg object-cover h-36" />
+        <Image src={map} alt="" className="rounded-lg object-cover h-36" placeholder="blur" priority/>
         <p className="absolute top-1/2 left-16 rounded-full bg-white/10 pl-2.5 pr-3 font-bold text-white/95 backdrop-blur-md">
           📍 Kolkata
         </p>
