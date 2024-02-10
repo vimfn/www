1904d03a6ab9c63c6dd1eef65a806bbd3826f747
Arunava Ghosh <arnvgh@gmail.com>
2024-02-10 09:53:16 +0530

    feat: 👾

--- app/(more)/books/page.tsx
+++ app/(more)/books/page.tsx
@@ -13,22 +13,21 @@ const booksPage = () => {
     <section>
       <div className="pb-10">
         <h1 className="text-2xl font-bold pb-8">Books</h1>
-        Aside from reading many lines of code, errors, and pages of
-        documentation, when I find some time or feel like delving into a topic
-        in depth, I often turn to books for their excellent in-depth
-        explanations.
-        <br />
-        <br />
-        If it is not something related to my work, or a mystery or sci-fi story
-        book or novel, I opt for the audiobook version and enjoy it either while
-        traveling or at bedtime.
-        <br />
-        <br />
+        <p className="mb-6">
+          Aside from reading many lines of code, errors, and pages of
+          documentation, when I find some time or feel like delving into a topic
+          in depth, I often turn to books for their excellent in-depth
+          explanations.
+        </p>
+        <p className="mb-6">
+          If it is not something related to my work, or a mystery or sci-fi
+          story book or novel, I opt for the audiobook version and enjoy it
+          either while traveling or at bedtime.
+        </p>
         {booksData.map(
           (book) => book.readingNow && <LatestBook key={book.url} {...book} />
         )}
-        <br />
-        <p>Below are some of my favorite collections of various authors.</p>
+        <p className="mt-6">Below are some of my favorite collections of various authors.</p>
         <div className="mt-8 grid md:grid-cols-2 gap-8">
           {booksData.map(
             (book) =>
