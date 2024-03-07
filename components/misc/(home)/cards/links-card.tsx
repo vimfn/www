0f59952c5bafdaad8be27afe5df80c0e93b2e611
Arunava Ghosh <arnvgh@gmail.com>
2024-03-07 15:39:39 +0530

    👾

--- components/misc/(home)/cards/links-card.tsx
+++ components/misc/(home)/cards/links-card.tsx
@@ -17,12 +17,12 @@ const links: LinksType[] = [
 const LinksCard = () => {
   return (
     <div className="flex gap-2">
-      <div className="h-[4.125rem] flex flex-col justify-center items-center rounded-lg text-white bg-[#2867b2] w-full">
+      <div className="hover:scale-95 duration-500 transform-gpu h-[4.125rem] flex flex-col justify-center items-center rounded-lg text-white bg-[#2867b2] w-full cursor-pointer">
         <Linkedin className=""/>
         {/* <span className="font-semibold items-center"></span> */}
         {/* <span className="text-sm">coding hrs</span> */}
       </div>
-      <div className="h-[4.125rem] flex flex-col justify-center items-center rounded-lg text-white bg-[#1c1d1f] w-full">
+      <div className="hover:scale-95 duration-500 cursor-pointer transform-gpu h-[4.125rem] flex flex-col justify-center items-center rounded-lg text-white bg-[#1c1d1f] w-full">
         <CalendarDays/>
       </div>
     </div>
--- components/misc/(home)/grid-cards.tsx
+++ components/misc/(home)/grid-cards.tsx
@@ -24,25 +24,33 @@ export const GridCards = () => {
       </div>
 
       <div className="grid md:grid-cols-6 grid-cols-3 gap-3 mt-3">
-      <div className="md:order-2 order-1">
-          <div className="flex flex-col gap-3">
-            {/* <CurrentTime /> */}
-            <LinksCard />
-            <WakatimeStats />
+        {/* <div className="md:order-2 order-1"> */}
+        {/* <CurrentTime /> */}
+
+        {/* </div> */}
+        {/* <LocateMe /> */}
+        <div className="flex flex-col col-span-3">
+          <div className="flex gap-3">
+            <div className="w-24">
+              <AnimeLinkCard />
+            </div>
+            <div className="flex flex-col gap-3 w-full ">
+              <LinksCard />
+              <WakatimeStats />
+            </div>
+          </div>
+
+          <div className="cols-span-3">
+            <StacksCard />
           </div>
         </div>
-        <div className="col-span-2 order-2">
-          {/* <LocateMe /> */}
-          <AnimeLinkCard />
-        </div>
+
         {/* <div className="col-span-1 order-2"> */}
         {/* </div>   */}
-       
+
         <div className="col-span-3">
-          {/* <StacksCard /> */}
           <ImagesCard />
         </div>
-        
       </div>
     </div>
   );
